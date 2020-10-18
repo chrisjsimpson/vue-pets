c59528a18b1a09c1457f304cb9792fa3e3c86d8e
chrisjsimpson <chris@karmacomputing.co.uk>
2020-10-18 21:53:00 +0100

    wip add cats/dogs crud

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import cats from '../data/cats'
+import dogs from '../data/dogs'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cats,
+    dogs,
+    pets: [...cats, ...dogs]
   },
   mutations: {
+    appendPet: (state, { species, pet }) => {
+      alert(species)
+      state[species].push(pet)
+    }
   },
   actions: {
+    addPet: ({ commit }, payload) => {
+      alert(payload)
+      commit('appendPet', payload)
+    }
   },
   modules: {
   }
